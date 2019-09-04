c8653afa3673de19df8f726ec99a474fa8dc9af7
MacDasgupta <lazymayukh@gmail.com>
2019-09-04 15:53:23 +0530

    Add files via upload

---
+++ SlidingWindowMax.java
@@ -0,0 +1,69 @@
+package DailyCodingProblem;
+
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class SlidingWindowMax {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int k = 3;
+        printKMax(arr, arr.length, k);
+        printMax(arr, arr.length, k);
+    }
+//Brute way
+    static void printKMax(int arr[],int len,int k){
+        int max;
+        for(int i=0;i<len-k;i++){
+            max=arr[i];
+            for(int j=1;j<k;j++){
+                if(arr[i+j]>max)
+                    max=arr[i+j];
+            }
+            System.out.println(max +" ");
+        }
+    }
+
+    static void printMax(int arr[], int n, int k)
+    {
+        // Create a Double Ended Queue, Qi that will store indexes of array elements
+        // The queue will store indexes of useful elements in every window and it will
+        // maintain decreasing order of values from front to rear in Qi, i.e.,
+        // arr[Qi.front[]] to arr[Qi.rear()] are sorted in decreasing order
+        Deque<Integer> Qi = new LinkedList<Integer>();
+
+        /* Process first k (or first window) elements of array */
+        int i;
+        for (i = 0; i < k; ++i) {
+            // For every element, the previous smaller elements are useless so
+            // remove them from Qi
+            while (!Qi.isEmpty() && arr[i] >= arr[Qi.peekLast()])
+                Qi.removeLast(); // Remove from rear
+
+            // Add new element at rear of queue
+            Qi.addLast(i);
+        }
+
+        // Process rest of the elements, i.e., from arr[k] to arr[n-1]
+        for (; i < n; ++i) {
+            // The element at the front of the queue is the largest element of
+            // previous window, so print it
+            System.out.print(arr[Qi.peek()] + " ");
+
+            // Remove the elements which are out of this window
+            while ((!Qi.isEmpty()) && Qi.peek() <= i - k)
+                Qi.removeFirst();
+
+            // Remove all elements smaller than the currently
+            // being added element (remove useless elements)
+            while ((!Qi.isEmpty()) && arr[i] >= arr[Qi.peekLast()])
+                Qi.removeLast();
+
+            // Add current element at the rear of Qi
+            Qi.addLast(i);
+        }
+
+        // Print the maximum element of last window
+        System.out.print(arr[Qi.peek()]);
+    }
+}
